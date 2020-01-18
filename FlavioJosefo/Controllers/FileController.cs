db2c91476919c397d04dafe873d5043147dc4382
JuanFelipeSalgadoDuque <juanfelipesalgadoduque@gmail.com>
2020-01-18 11:00:09 -0500

    Reading a .txt File

---
+++ FlavioJosefo/Controllers/FileController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlavioJosefo.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file, int jump)
+        {
+            try
+            {
+                //Read all file
+                string result = new StreamReader(file.InputStream).ReadToEnd();
+
+                //Split the names in file for \n
+                string[] data = result.Split('\n');
+
+                //Mi otro controlador necesita 2 parámetros
+                return RedirectToAction("Datos", new { nom = data[0].ToString(), mail = data[1].ToString() });
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.mensaje = "Se produjo un error : " + ex.Message;
+            }
+
+            return View("Index");
+        }
+    }
+}
